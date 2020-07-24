51617d240b2cfcf9b8944b15a44308e267ec90c3
Lizard <konovv.nikk@gmail.com>
2020-07-24 14:30:43 +0300

    webhook

--- bot.py
+++ bot.py
@@ -1,25 +1,75 @@
 import logging
-from config import bot
 import handlers
+import ssl
+import telebot
+from aiohttp import web
 
-def main():
-    logger = logging.getLogger('Bot')
+API_TOKEN = "1133045935:AAGwz37J1uFoE_wTIMnoTuy7KLuw_bX_E54"
 
-    # Create handlers
-    f_handler = logging.FileHandler(filename='Logs/botlog.log', mode='w')
-    f_handler.setLevel(logging.INFO)
+WEBHOOK_HOST = '<ip/host where the bot is running>'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-    # Create formatters and add it to handlers
-    f_format = logging.Formatter( fmt= '%(asctime)s - %(name)s - %(funcName)s: %(message)s',
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+logger = logging.getLogger('Bot')
+# Create handlers
+f_handler = logging.FileHandler(filename='Logs/botlog.log', mode='w')
+f_handler.setLevel(logging.INFO)
+# Create formatters and add it to handlers
+f_format = logging.Formatter( fmt= '%(asctime)s - %(name)s - %(funcName)s: %(message)s',
                                 datefmt= '%d-%M-%Y %H:%M:%S')
-    f_handler.setFormatter(f_format)
+f_handler.setFormatter(f_format)
+# Add handlers to the logger
+logger.addHandler(f_handler)
 
-    # Add handlers to the logger
-    logger.addHandler(f_handler)
-
-    logger.error('Bot started')
     
-    bot.polling(none_stop=True)
+bot = telebot.TeleBot(API_TOKEN)
 
-if __name__ == "__main__":
-    main()
+app = web.Application()
+
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+
+app.router.add_post('/{token}/', handle)
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+logger.error('Bot started')
+
+# Build ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+# Start aiohttp server
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_context=context,
+)
