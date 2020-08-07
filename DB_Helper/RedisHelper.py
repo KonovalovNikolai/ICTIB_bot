9d5221a5ae0780107dfd27e43e07edacaa98f60c
Lizard <konovv.nikk@gmail.com>
2020-08-07 16:24:30 +0300

    for the server

--- DB_Helper/RedisHelper.py
+++ DB_Helper/RedisHelper.py
@@ -1,17 +1,19 @@
 import redis
 
+PASS = 'QzEcTb123789'
+
 def get_current_state(user_id):
-    with redis.Redis() as db:
+    with redis.Redis(password= PASS) as db:
         return db.get(user_id).decode('utf-8')
 
 def set_state(user_id, value):
-    with redis.Redis() as db:
+    with redis.Redis(password= PASS) as db:
         db.set(user_id, value)
 
 def delet_user(user_id):
-    with redis.Redis() as db:
+    with redis.Redis(password= PASS) as db:
         db.delete(user_id)
         
 def get_message(value):
-    with redis.Redis(db=1) as db:
+    with redis.Redis(db=1, password= PASS) as db:
         return db.get(value).decode('utf-8')
