039813bd3d728233212e370cbaba715297bedd96
Lizard <konovv.nikk@gmail.com>
2020-08-22 19:40:49 +0300

    Polling update

--- handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import start_handlers
-from . import back_handler
-from . import clear_handlers
-from . import timetable_handlers
-from . import questions_handler
-from . import vk_handler
-from . import settings_handler
-from . import buildings_handler
-
